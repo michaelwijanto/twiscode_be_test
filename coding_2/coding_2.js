360343204d2e7b4292dde04635690756c7683e86
michaelwijanto <michael.wijanto@gmail.com>
2022-03-10 14:31:39 +0700

    coding 2

--- coding_2/coding_2.js
+++ coding_2/coding_2.js
@@ -1,5 +1,20 @@
 function palindrome(text) {
   const rexp = /[^A-Za-z0-9]/g;
-  const formatted = text.toLowerCase().replace(rexp, "");
-  const reversed = formatted.split("").reverse().join("");
+  const arr = text.split(" ");
+  let palindrome = "";
+  let formatted = "";
+  let reversed = "";
+  for (let i = 0; i < arr.length - 1; i++) {
+    formatted = (arr[i] + arr[i + 1]).toLowerCase().replace(rexp, "");
+    reversed = formatted.split("").reverse().join("");
+    if (formatted == reversed) {
+      palindrome = arr[i] + " " + arr[i + 1];
+    }
+  }
+  return palindrome;
 }
+
+console.log(palindrome("aku suka"));
+console.log(palindrome("aku suka makan nasi"));
+console.log(palindrome("di rumah saya ada kasur rusak"));
+// console.log(palindrome("abcde edcbza"));
